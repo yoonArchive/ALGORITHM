292b9bd712bd4cfe6f7193ceb2b7bd8452346696
yoonArchive <yoonarchive1@gmail.com>
2022-02-14 18:25:30 +0900

    Create SWEA6808

---
+++ SWEA/Solution_6808_D3_규영이와인영이의카드게임/Solution_6808_D3_규영이와인영이의카드게임.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_D3_6808_규영이와인영이의카드게임 {
+
+	public static int kyuWin, kyuLose;
+	public static int[] kyuCards = new int[10];
+	public static boolean[] isSelected;
+
+	public static void game(int cnt, int kyuSum, int inSum) { //순열
+
+		if (cnt == 9) {
+			if (kyuSum > inSum)
+				kyuWin++;
+			else
+				kyuLose++;
+			return;
+		}
+
+		for (int n = 1; n <= 18; n++) {
+			if (isSelected[n])
+				continue;
+
+			int sum = kyuCards[cnt + 1] + n;
+			isSelected[n] = true;
+			if (kyuCards[cnt + 1] > n)
+				game(cnt + 1, kyuSum + sum, inSum);
+			else
+				game(cnt + 1, kyuSum, inSum + sum);
+			isSelected[n] = false;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("s_input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			isSelected = new boolean[19];
+
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 1; i <= 9; i++) {
+				int num = Integer.parseInt(st.nextToken());
+				kyuCards[i] = num;
+				isSelected[num] = true;
+			}
+			kyuWin = 0;
+			kyuLose = 0;
+			game(0, 0, 0);
+			sb.append("#" + tc + " " + kyuWin + " " + kyuLose + "\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
---
+++ SWEA/Solution_6808_D3_규영이와인영이의카드게임/Solution_6808_D3_규영이와인영이의카드게임_비트마스킹ver.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_6808_D3_규영이와인영이의카드게임_비트마스킹ver {
+
+	public static int kyuWin, kyuLose;
+	public static int[] kyuCards = new int[10];
+
+	public static void game(int cnt, int kyuSum, int inSum, int flag) { // flag를 매개변수에 추가
+
+		if (cnt == 9) {
+			if (kyuSum > inSum)
+				kyuWin++;
+			else
+				kyuLose++;
+			return;
+		}
+
+		for (int n = 1; n <= 18; n++) {
+			if ((flag & 1 << n) != 0)
+				continue;
+
+			int sum = kyuCards[cnt + 1] + n;
+			if (kyuCards[cnt + 1] > n)
+				game(cnt + 1, kyuSum + sum, inSum, flag | 1 << n);
+			else
+				game(cnt + 1, kyuSum, inSum + sum, flag | 1 << n);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("s_input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			int flag = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 1; i <= 9; i++) {
+				int num = Integer.parseInt(st.nextToken());
+				kyuCards[i] = num;
+				flag = flag | 1 << num;
+			}
+			kyuWin = 0;
+			kyuLose = 0;
+			game(0, 0, 0, flag);
+			sb.append("#" + tc + " " + kyuWin + " " + kyuLose + "\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
